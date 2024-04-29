b7328f4acb1f0d033ff928af6a64bda615cd594c
Husslim <163634394+Husslim@users.noreply.github.com>
2024-04-29 22:09:40 +1000

    formkit installed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
+import { plugin, defaultConfig } from '@formkit/vue'
+
+
 import App from './App.vue'
 import router from './router'
-
+createApp(App).use(plugin, defaultConfig).mount('#app')
 const app = createApp(App)
 
 app.use(createPinia())
