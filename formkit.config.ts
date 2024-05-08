5cecbb545f4d0782ffbb6d7ec73e21a5d437421f
Husslim <163634394+Husslim@users.noreply.github.com>
2024-05-08 17:56:07 +1000

    formkit installation

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,7 +1,8 @@
-import { fr } from '@formkit/i18n'
 import { defaultConfig } from '@formkit/vue'
+import { rootClasses } from './formkit.theme'
 
 export default defaultConfig({
-  locales: { fr },
-  locale: 'fr',
+  config: {
+    rootClasses,
+  },
 })
