75f75ceb860d752b2ba176ad23cf0c6c0dee4b34
peterjaberau <peterjaberau@gmail.com>
2024-05-08 19:12:17 +1000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-import config from 'formkit.config.ts'
+<script setup lang="ts">
+import config from './formkit.config.ts'
 
 app.use(plugin, defaultConfig(config))
+
+</script>
 <template>
   <h1 class="text-3xl font-bold underline">
     Hello world!
