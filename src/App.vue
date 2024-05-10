7003c20bb6dd84bbb871a840150f27cd1f553eee
Husslim <163634394+Husslim@users.noreply.github.com>
2024-05-10 21:40:45 +1000

    tailwind - import input file into main testing

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts">
-
-</script>
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">
+      Hello 👋🏼
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
