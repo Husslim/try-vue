4c97c707259ba3e10c2f257bf67c55baa89b3ae9
Husslim <163634394+Husslim@users.noreply.github.com>
2024-05-14 20:14:04 +1000

    mabroke added into the screen

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 <template>
   <div class="justify-center flex bg-yellow-300 items-center h-screen">
     <div class="text-4xl underline">
-      Hello 👋🏼
+      Hello mabroke 👋🏼
     </div>
   </div>
 </template>
