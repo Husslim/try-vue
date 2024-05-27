d4f25634c18ae73627547ce93a850babbb14a54b
Husslim <163634394+Husslim@users.noreply.github.com>
2024-05-27 21:03:33 +1000

    rendering error in app line 6

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 
-import basicMultistep from './components/formkits/basicMultistep.vue'
+//import basicMultistep from './components/formkits/basicMultistep.vue'
 //import introMultistep from './components/formkits/introMultistep.vue';
 //import basicMultistep from './components/formkits/basicMultistep.vue'
-
-
+import example from './components/valikits/example'
+//when rendering im getting this error Module '"d:/projects/try-vue/src/components/valikits/example"' has no default export on ln 6
 </script>
 
 
 <template>
- <basicMultistep>   sadfj
+ <example>   sadfj
 
- </basicMultistep>
+ </example>
 </template>
