a88b2e364da27c56ba7b9d3ca41f434c53a63acf
Husslim <163634394+Husslim@users.noreply.github.com>
2024-05-27 22:01:30 +1000

    feature rendering error - white screen

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 
 //import basicMultistep from './components/formkits/basicMultistep.vue'
-//import introMultistep from './components/formkits/introMultistep.vue';
+import introMultistep from './components/formkits/introMultistep.vue'
 //import basicMultistep from './components/formkits/basicMultistep.vue'
-import example from './components/valikits/example.vue'
-//when rendering im getting this error Module '"d:/projects/try-vue/src/components/valikits/example"' has no default export on ln 6
+//import example from './components/valikits/example.vue'
 </script>
 
 
 <template>
- <example>   sadfj
-
- </example>
+ <introMultistep> 
+fgsg
+ </introMultistep>
 </template>
