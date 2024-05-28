7f8ef6f090882413c1f995dd6720272fa5e17f14
Husslim <163634394+Husslim@users.noreply.github.com>
2024-05-28 16:50:38 +1000

    code cleaned and deleted unneccessary files

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 
-//import basicMultistep from './components/formkits/basicMultistep.vue'
 import IntroMultistep from './components/formkits/introMultistep.vue'
-//import basicMultistep from './components/formkits/basicMultistep.vue'
-//import example from './components/valikits/example.vue'
-//import StepOne from './components/formkits/StepOne.vue';
+
 </script>
 
 
